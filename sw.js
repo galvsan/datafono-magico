84398bca11f4d47f1bd70c4c5663dff3a9bb13b1
galvsan <33752032+galvsan@users.noreply.github.com>
2026-03-23 09:44:32 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,31 @@
-const CACHE_NAME = 'bankplay-v1';
+const CACHE_NAME = 'bankplay-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './products.json',
-  'https://cdn.tailwindcss.com'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
